e962c338adc88a149744128ef076f0ce6ac26436
sopheaknova <novadesign.dev@gmail.com>
2014-11-17 16:55:23 +0700

    Show title page excerpt homepage

--- page.php
+++ page.php
@@ -12,12 +12,17 @@
     <div class="main">
 		<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+				
+				<?php if ( !is_front_page() ) : ?>
 				<header class="entry-header">
 					<h1 class="entry-title"><?php the_title(); ?></h1>
 				</header>
+				<?php endif; ?>
+				
 				<div class="entry-content">
 					<?php the_content(); ?>
 				</div><!-- .entry-content -->
+
 			</article><!-- #post -->
 		<?php endwhile;
 		else : 
